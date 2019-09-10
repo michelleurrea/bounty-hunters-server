2fcd5276b523a8137df1658d866f34f717c0ce3a
michelleurrea <michelleurrea@gmail.com>
2019-09-10 11:46:40 -0700

    About to deploy partial app

---
+++ controllers/v1/bounties.js
@@ -0,0 +1,49 @@
+// Create router and reference to models
+let db = require('../../models')
+let router = require('express').Router()
+
+// Get /v1/bounties
+router.get('/', (req, res) => {
+	db.Bounty.find()
+	.then(bounties => {
+		res.send(bounties)
+	})
+	.catch(err => {
+		console.log(err)
+		res.status(503).send({ message: 'Database asleep?' })
+	})
+})
+
+// GET /v1/bounties/:id
+router.get('/:id', (req, res) => {
+	res.send('STUB for GET /bounties/:id')
+})
+
+// POST /v1/bounties
+router.post('/', (req, res) => {
+	db.Bounty.create(req.body)
+	.then(bounty => {
+		res.status(201).send(bounty)
+	})
+	.catch(err => {
+		console.log('Error while creating', err)
+		res.status(500).send({ message: 'Server error' })
+	})
+})
+
+// PUT /v1/bounties/:id
+router.put('/:id', (req, res) => {
+	res.send('STUB for PUT /bounties/:id')
+})
+
+// DELETE /v1/bounties
+router.delete('/', (req, res) => {
+	res.send('STUB for DELETE /bounties')
+})
+
+// DELETE /v1/bounties/:id
+router.delete('/:id', (req, res) => {
+	res.send('STUB for DELETE /bounties/:id')
+})
+
+module.exports = router
---
+++ models/bounty.js
@@ -0,0 +1,33 @@
+let mongoose = require('mongoose')
+
+// Create a bounty schema
+let bountySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minLength: 2,
+		maxlength: 100
+	},
+	wantedFor: {
+		type: String,
+		required: true
+	},
+	client: {
+		type: String,
+		required: true
+	},
+	ship: String,
+	reward: {
+		type: Number,
+		default: 100000
+	},
+	hunters: Array,
+	captured: {
+		type: Boolean,
+		default: false
+	},
+	lastSeen: String
+})
+
+// Export the model
+module.exports = mongoose.model('Bounty', bountySchema)
